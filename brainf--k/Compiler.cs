f6aea34a412a465e23df27713ad8571095b79a43
David Mason <nerdcorerising@gmail.com>
2011-03-10 16:00:57 -0500

    setting up repo

---
+++ brainf--k/Compiler.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace brainf__k
+{
+    class Compiler
+    {
+        List<char> symbols = null;
+
+        void readFile(string input)
+        {
+            //The valid characters in the language
+            HashSet<char> valid = new HashSet<char>(new List<char> { '<', '>', '+', '-', '.', ',', '[', ']' });
+            //Open the file
+            //TODO add checks and stuff here.
+            System.IO.StreamReader read = System.IO.File.OpenText(input);
+
+            //Get all the characters from the file
+            String line = read.ReadLine();
+            while (line != null)
+            {
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (valid.Contains(line[i]))
+                    {
+                        this.symbols.Add(line[i]);
+                    }
+                }
+                line = read.ReadLine();
+            }
+        }
+
+        void compileFile(string output)
+        {
+            //TODO compile the file and stuff.
+        }
+    }
+}
---
+++ brainf--k/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace brainf__k
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
