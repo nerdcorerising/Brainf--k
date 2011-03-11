9f38ee84181cb71122f848dc94c71f1b6be98171
David Mason <nerdcorerising@gmail.com>
2011-03-10 22:23:19 -0500

    implemented, some bug testing left to do.

--- brainf--k/Compiler.cs
+++ brainf--k/Compiler.cs
@@ -7,15 +7,20 @@ namespace brainf__k
 {
     class Compiler
     {
-        List<char> symbols = null;
+        private static int SIZE = 30000;
 
-        void readFile(string input)
+        private List<char> Symbols = null;
+        private Stack<int> Loop = null;
+        private byte[] Memory = null;
+        private int ExecutionPointer = 0;
+        private int Pointer = 0;
+
+        public void ReadFile(System.IO.StreamReader read)
         {
             //The valid characters in the language
             HashSet<char> valid = new HashSet<char>(new List<char> { '<', '>', '+', '-', '.', ',', '[', ']' });
-            //Open the file
-            //TODO add checks and stuff here.
-            System.IO.StreamReader read = System.IO.File.OpenText(input);
+
+            this.Symbols = new List<char>();
 
             //Get all the characters from the file
             String line = read.ReadLine();
@@ -25,14 +30,81 @@ namespace brainf__k
                 {
                     if (valid.Contains(line[i]))
                     {
-                        this.symbols.Add(line[i]);
+                        this.Symbols.Add(line[i]);
                     }
                 }
                 line = read.ReadLine();
             }
         }
 
-        void compileFile(string output)
+        public void Interpret()
+        {
+            //Return if no program loaded.
+            if (this.Symbols == null) return;
+
+            //Initialize memory.
+            this.Memory = new byte[SIZE];
+            this.Loop = new Stack<int>();
+
+            //Execute the program.
+            for (this.ExecutionPointer = 0; this.ExecutionPointer < this.Symbols.Count; this.ExecutionPointer++)
+            {
+                switch (this.Symbols[this.ExecutionPointer])
+                {
+                    case '<':
+                        this.Pointer--;
+                        break;
+                    case '>':
+                        this.Pointer++;
+                        break;
+                    case '+':
+                        this.Memory[this.Pointer]++;
+                        break;
+                    case '-':
+                        this.Memory[this.Pointer]--;
+                        break;
+                    case '.':
+                        System.Console.Write((char)this.Memory[this.Pointer]);
+                        break;
+                    case ',':
+                        this.Memory[this.Pointer] = (byte)System.Console.Read();
+                        break;
+                    case '[':
+                        if (this.Memory[this.Pointer] != 0)
+                        {
+                            this.Loop.Push(this.ExecutionPointer);
+                        }
+                        else
+                        {
+                            int inside = 0;
+                            while (this.Symbols[++this.ExecutionPointer] != ']' || inside > 0)
+                            {
+                                if (this.Symbols[this.ExecutionPointer] == '[')
+                                {
+                                    inside++;
+                                }
+                                if (this.Symbols[this.ExecutionPointer] == ']')
+                                {
+                                    inside--;
+                                }
+                            }
+                        }
+                        break;
+                    case ']':
+                        if (this.Memory[this.Pointer] != 0)
+                        {
+                            this.ExecutionPointer = this.Loop.Peek();
+                        }
+                        else
+                        {
+                            this.Loop.Pop();
+                        }
+                        break;
+                }
+            }
+        }
+
+        public void CompileToFile(string output)
         {
             //TODO compile the file and stuff.
         }
--- brainf--k/Program.cs
+++ brainf--k/Program.cs
@@ -9,6 +9,17 @@ namespace brainf__k
     {
         static void Main(string[] args)
         {
+            System.IO.StreamReader inputFile = new System.IO.StreamReader(args[0]);
+            Compiler c = new Compiler();
+            c.ReadFile(inputFile);
+            try
+            {
+                c.Interpret();
+            }
+            catch (IndexOutOfRangeException i)
+            {
+                System.Console.Error.Write("Brainfault.");
+            }
         }
     }
 }
